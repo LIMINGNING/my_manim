75f1f192de46606a4f014a7e29ac037de1e40d52
MINGNING <2664135882@qq.com>
2025-04-21 14:01:03 +0800

    add GROUND_WIDTH... to config.py

--- config.py
+++ config.py
@@ -5,3 +5,17 @@ config.pixel_height = 1080
 config.pixel_width = 1920
 config.frame_height = 7.0
 config.frame_width = 12.0
+
+GROUND_WIDTH = (config.frame_height - 0.1)*37/100
+GROUND_LENGTH = config.frame_height - 0.1
+GROUND_RATIO = 1.5
+PLAER_RADIUS = 0.15
+FRISBEE_RADIUS = 0.1
+BIGGER_GROUND_LENGTH = GROUND_RATIO * GROUND_LENGTH
+BIGGER_GROUND_WIDTH = GROUND_RATIO * GROUND_WIDTH
+
+# 标准化的对角线方向 (长度为1)
+UR_UNIT = normalize(UR)
+UL_UNIT = normalize(UL)
+DR_UNIT = normalize(DR)
+DL_UNIT = normalize(DL)
